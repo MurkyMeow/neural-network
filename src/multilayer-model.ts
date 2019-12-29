b70b9198a86485fc39dd72b512d88560b10be3db
MurkyMeow <dinosowermurky@gmail.com>
2019-12-29 17:31:04 +0900

    multilayer-model: rewrite

--- src/multilayer-model.ts
+++ src/multilayer-model.ts
@@ -1,64 +1,104 @@
- import { dot } from './helper'
+import { Matrix, matdot } from './helper'
 
-const make_model = (
-  layers: number[][],
-  outputs: number[][],
-  learning_rate: number = 0.01,
-) => ({
-  guess(inputs: number[]): number[] {
-    const hidden = layers.map(layer => dot(layer, inputs))
-    return outputs.map(output => dot(output, hidden))
-  },
-  learn(guess: number[], actual: number[]) {
-    const out_errors = actual.map((val, i) => val - guess[i])
-    const new_outputs = outputs.map((output, i) => {
-      return output.map(w => w + out_errors[i] * actual[i])
-    })
-    const hidden_errors = layers.map(layer => {
-      return dot(layer, out_errors)
-    })
-    const new_layers = layers.map((layer, i) => {
-      return layer.map(w => w + hidden_errors[i] * actual[i])
-    })
-    return make_model(new_layers, new_outputs, learning_rate)
-  },
-})
+const normalize = (x: number): number =>
+  1 / (1 - Math.exp(-x))
+
+const get_change_rate = (x: number): number =>
+  normalize(x) * (1 - normalize(x))
+
+interface Model<
+  InputSize extends number,
+  OutputSize extends number,
+  LayersCount extends number,
+> {
+  layers: Matrix<LayersCount, InputSize>
+  outputs: Matrix<OutputSize, LayersCount>
+  learningRate: number
+}
+
+function guess<I extends number, O extends number, L extends number>(
+  model: Model<I, O, L>,
+  inputs: Matrix<I, 1>,
+): Matrix<O, 1> {
+  const hidden = matdot(model.layers, inputs)
+  return matdot(model.outputs, hidden)
+}
+
+function learn<I extends number, O extends number, L extends number>(
+  model: Model<I, O, L>,
+  guess: Matrix<O, 1>,
+  actual: Matrix<O, 1>,
+): Model<I, O, L> {
+  const out_errors = actual.map((val, i) => (val - guess[i]) ** 2)
+  const out_delta = guess
+    .map(get_change_rate)
+    .map((direction, i) => direction * out_errors[i] * model.learningRate)
+  const new_outputs = model.outputs
+    .map((output, i) => output.map(w => w - out_delta[i]))
+  return { ...model, outputs: new_outputs }
+}
 
 // ---- PREDICTING XOR ----
 
-const data = [{
-  input: [1, 1],
-  expected: [0],
+interface Chunk {
+  input: Matrix<2, 1>
+  expected: Matrix<1, 1>
+}
+
+const data: Chunk[] = [{
+  input: [
+    [1],
+    [1],
+  ],
+  expected: [
+    [0],
+  ],
 }, {
-  input: [1, 0],
-  expected: [1],
+  input: [
+    [1],
+    [0],
+  ],
+  expected: [
+    [1]
+  ],
 }, {
-  input: [0, 1],
-  expected: [1],
+  input: [
+    [0],
+    [1],
+  ],
+  expected: [
+    [1],
+  ],
 }, {
-  input: [0, 0],
-  expected: [0],
+  input: [
+    [0],
+    [0],
+  ],
+  expected: [
+    [1],
+  ],
 }]
 
-let model = make_model(
-  [
+let model: Model<2, 1, 3> = {
+  learningRate: 0.01,
+  layers: [
     [1, 1],
     [1, 1],
     [1, 1],
   ],
-  [
+  outputs: [
     [1, 1, 1],
   ],
-)
+}
 
 // Train
 for (let i = 0; i < 1000; i++) {
   const chunk = data[Math.random() * data.length | 0]
-  const guess = model.guess(chunk.input)
-  model = model.learn(guess, chunk.expected)
+  const prediction = guess(model, chunk.input)
+  model = learn(model, prediction, chunk.expected)
 }
 
 // Test
 for (const chunk of data) {
-  console.log(chunk.input, model.guess(chunk.input))
+  console.log(chunk.input, guess(model, chunk.input))
 }
